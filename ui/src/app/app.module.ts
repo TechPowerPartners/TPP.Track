e0d0f6a341ddcb1cd523f4600e648b1056691ca5
Maksim Vorobev <maks233online@gmail.com>
2023-06-16 19:55:10 +0300

    F-1 Добавил тестовый таймер. Вынес стили

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
