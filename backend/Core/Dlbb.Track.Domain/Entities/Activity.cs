909e2208001c3529ce3a166b7f0ed67376ba078d
Maksim Vorobev <maks233online@gmail.com>
2023-06-16 23:21:13 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ backend/Core/Dlbb.Track.Domain/Entities/Activity.cs
@@ -0,0 +1,11 @@
+﻿using Dlbb.Track.Domain.Entities.Base;
+
+namespace Dlbb.Track.Domain.Entities
+{
+    public class Activity : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        public List<Session> Sessions { get; } = new();
+    }
+}
---
+++ backend/Infrastructure/Dlbb.Track.Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Dlbb.Track.Domain.Entities;
+using Dlbb.Track.Persistence.EntityTypeConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dlbb.Track.Persistence.Contexts;
+public class AppDbContext : DbContext
+{
+	public DbSet<Session> Sessions { get; set; }
+	public DbSet<Activity> Activities { get; set; }
+
+	public AppDbContext(DbContextOptions<AppDbContext> options)
+		: base(options)
+	{
+
+	}
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder.ApplyConfiguration(new ActivityTypeConfiguration());
+		builder.ApplyConfiguration(new SessionTypeConfiguration());
+		base.OnModelCreating(builder);
+	}
+}
