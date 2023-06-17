9a55403e034a8303a4d38c483bfd9401af21e426
razil <razil_khayka@mail.ru>
2023-06-17 21:08:11 +0300

    B-3 исправил program, удалил WeatherForecast

--- backend/Presentation/Dlbb.Track.WebApi/Program.cs
+++ backend/Presentation/Dlbb.Track.WebApi/Program.cs
@@ -1,67 +1,64 @@
 ﻿using Dlbb.Track.Application.Common.Mappings;
+using Dlbb.Track.Application.CompositionRoot;
 using Dlbb.Track.Persistence.CompositionRoot;
 using Dlbb.Track.WebApi.Mappings;
 using Dlbb.Track.WebApi.SignalRHub;
 
 
-namespace Dlbb.Track.WebApi
+namespace Dlbb.Track.WebApi;
+
+public class Program
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-			// Add services to the container.
-			builder.Services.AddEf(builder.Configuration);
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+		builder.Services.AddEf(builder.Configuration);
+		builder.Services.AddApplication();
 
-			builder.Services.AddAutoMapper(config =>
+		builder.Services.AddAutoMapper(config =>
+		{
+			config.AddProfile(new ApplicationMappingProfile());
+			config.AddProfile(new WebApiMappingProfile());
+		});
+
+		builder.Services.AddControllers();
+
+		builder.Services.AddSignalR();
+
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen(options => options.AddSignalRSwaggerGen());
+
+		builder.Services.AddCors(options =>
+		{
+			options.AddPolicy("AllowAll", policy =>
 			{
-				config.AddProfile(new ApplicationMappingProfile());
-				config.AddProfile(new WebApiMappingProfile());
+				policy.AllowAnyHeader();
+				policy.AllowAnyMethod();
+				policy.AllowAnyOrigin();
 			});
+		});
 
-            builder.Services.AddControllers();
-			builder.Services.AddControllers();
-			builder.Services.AddSignalR();
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
 
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen(options => options.AddSignalRSwaggerGen());
-            builder.Services.AddControllers();
-            builder.Services.AddControllers();
-            builder.Services.AddControllers();
+		var app = builder.Build();
 
-			builder.Services.AddCors(options =>
-			{
-				options.AddPolicy("AllowAll", policy =>
-				{
-					policy.AllowAnyHeader();
-					policy.AllowAnyMethod();
-					policy.AllowAnyOrigin();
-				});
-			});
-
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
-
-			var app = builder.Build();
-
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
-
-			app.UseHttpsRedirection();
-
-			app.UseAuthorization();
-
-			app.MapHub<TimerHub>("/timerhub");
-
-			app.UseCors("AllowAll");
-
-			app.MapControllers();
-
-			app.Run();
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
 		}
+
+		app.UseHttpsRedirection();
+
+		app.UseAuthorization();
+
+		app.MapHub<TimerHub>("/timerhub");
+
+		app.UseCors("AllowAll");
+
+		app.MapControllers();
+
+		app.Run();
 	}
 }
