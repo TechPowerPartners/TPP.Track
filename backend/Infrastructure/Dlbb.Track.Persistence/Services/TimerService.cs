978ae991c435dbd2d707f52c12ffb9d13ff9a02c
Dolbab1337 <geekasdfg1337@gmai.com>
2023-06-25 19:37:56 +0200

    B-11 fix something

--- backend/Infrastructure/Dlbb.Track.Persistence/Services/TimerService.cs
+++ backend/Infrastructure/Dlbb.Track.Persistence/Services/TimerService.cs
@@ -3,22 +3,22 @@
 namespace Dlbb.Track.Persistence.Services;
 public class TimerService : ITimerService
 {
-  private Stopwatch _stopwatch = new();
+	private Stopwatch _stopwatch = new();
 
-  public string Time => _stopwatch.Elapsed.ToString();
+	public string Time => _stopwatch.Elapsed.ToString();
 
-  public void Reset()
-  {
-	_stopwatch.Reset();
-  }
+	public void Reset()
+	{
+		_stopwatch.Reset();
+	}
 
-  public void Start()
-  {
-	_stopwatch.Start();
-  }
+	public void Start()
+	{
+		_stopwatch.Start();
+	}
 
-  public void Stop()
-  {
-	_stopwatch.Stop();
-  }
+	public void Stop()
+	{
+		_stopwatch.Stop();
+	}
 }
