8dfbbb0eab8a7610f0608a017c8e7dec8d3c3077
Dolbab1337 <geekasdfg1337@gmai.com>
2023-06-25 06:51:30 +0200

    B-11 made prototype TimerService for 1 user

---
+++ backend/Infrastructure/Dlbb.Track.Persistence/Services/TimerService.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace Dlbb.Track.Persistence.Services;
+public class TimerService : ITimerService
+{
+  private Stopwatch _stopwatch = new();
+
+  public string Time => _stopwatch.Elapsed.ToString();
+
+  public void Reset()
+  {
+	_stopwatch.Reset();
+  }
+
+  public void Start()
+  {
+	_stopwatch.Start();
+  }
+
+  public void Stop()
+  {
+	_stopwatch.Stop();
+  }
+}
