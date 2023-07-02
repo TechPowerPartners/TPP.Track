9f1ce8d848956fd9efac946864e793f10a9c3cf0
Dolbab1337 <geekasdfg1337@gmai.com>
2023-07-02 11:17:48 +0200

    B-20 move configurations to startup

---
+++ backend/Presentation/Dlbb.Track.WebApi/Startup/AddAuthorizationExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using Dlbb.Track.Domain.Enums;
+
+namespace Dlbb.Track.WebApi.Startup;
+
+public static class AddAuthorizationExtension
+{
+	public static IServiceCollection AddAuthorizationConfiguration(this IServiceCollection services)
+	{
+		services.AddAuthorization((opt) =>
+		{
+			opt.AddPolicy("Admin", p =>
+				p.RequireAssertion(x => x.User.HasClaim(ClaimTypes.Role, RoleEnum.Admin.ToString())
+										|| x.User.HasClaim(ClaimTypes.Role, RoleEnum.User.ToString())));
+			opt.AddPolicy("User", p =>
+				p.RequireAssertion(x => x.User.HasClaim(ClaimTypes.Role, RoleEnum.User.ToString())));
+		});
+
+		return services;
+	}
+}
---
+++ backend/Presentation/Dlbb.Track.WebApi/Startup/UseSingnalRExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace Dlbb.Track.WebApi.Startup;
+
+public static class UseSingnalRExtension
+{
+	public static IServiceCollection UseSignalR(this IServiceCollection services)
+	{
+		services.AddSignalR(opt =>
+		{
+			opt.EnableDetailedErrors = true;
+		});
+
+		return services;
+	}
+}
