d831260be34adb6a611a3f0f4136aa1fc020b76b
razil <razil_khayka@mail.ru>
2023-07-17 01:16:13 +0300

    Validator for Actitvity

---
+++ backend/Presentation/Dlbb.Track.WebApi/Models/Validators/CreateAcitivityDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using Dlbb.Track.WebApi.Models.Activities;
+using FluentValidation;
+
+namespace Dlbb.Track.WebApi.Models.Validators;
+
+public class CreateActivityDtoValidator : AbstractValidator<CreateActivityDto>
+{
+	public CreateActivityDtoValidator()
+	{
+		RuleFor(x => x.Name).NotEmpty().WithMessage("Обязательно для заполнения");
+		RuleFor(x => x.Name).Length(1, 250);
+		RuleFor(x => x.Description).Length(0, 250);
+	}
+}
